209fd9af43f7f8620abe54ee9bc841acba54aff8
Faustino Aguilar <faustinoaq@gmail.com>
2017-12-01 20:11:47 -0500

    Add new port config

--- config/application.cr
+++ config/application.cr
@@ -5,5 +5,5 @@ require "../src/channels/*"
 require "../src/sockets/*"
 
 Amber::Server.configure do |settings|
-  settings.port = (ENV["PORT"] || 3000).to_i
+  settings.port = ENV["PORT"].to_i if ENV["PORT"]?
 end
