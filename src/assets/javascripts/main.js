20035aff7c1e9a6113ce0cc27e0a6989391691f2
Faustino Aguilar <faustinoaq@gmail.com>
2017-12-04 03:22:33 -0500

    Add new features
    - Avoid duplicate users
    - Use localStorage API
    - Use handle_joined and handle_leave

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -2,8 +2,10 @@ import Amber from 'amber';
 
 let socket = new Amber.Socket('/chat');
 
-let messages = document.getElementById('messages');
-let message = document.getElementById('message');
+let message = $('#message')[0];
+let messages = $('#messages')[0];
+
+localStorage.setItem('user', $('.user').text());
 
 socket.connect().then(() => {
     let channel = socket.channel('chat_room:hello');
@@ -13,17 +15,30 @@ socket.connect().then(() => {
     channel.on('message_new', payload => {
         let p = document.createElement('p');
         p.innerText = payload.message;
-        p.innerHTML = `<b>${payload.user.trim()}: </b>` + p.innerText;
+        if (payload.user.length > 0) {
+            p.innerHTML = `<b>${payload.user.trim()}: </b>` + p.innerText;
+        }
         messages.append(p);
         messages.scrollTop = messages.scrollHeight;
     });
 
     $('#form-message').on('submit', event => {
         event.preventDefault();
-        channel.push('message_new', {
-            user: $('.user').text(),
-            message: message.value
-        });
-        message.value = '';
+        if (localStorage.getItem('user') === null) {
+            let p = document.createElement('p');
+            p.innerText = "You are disconnected!";
+            messages.append(p);
+        } else {
+            channel.push('message_new', {
+                user: localStorage.getItem('user'),
+                message: message.value
+            });
+            message.value = '';
+        }
+    });
+
+    $('.logout').on('click', event => {
+        socket.disconnect();
+        localStorage.clear();
     });
 });
--- src/channels/chat_room_channel.cr
+++ src/channels/chat_room_channel.cr
@@ -1,5 +1,9 @@
 class ChatRoomChannel < Amber::WebSockets::Channel
   def handle_joined(client_socket, message)
+    ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
+      "user"    => "",
+      "message" => "#{client_socket.session["user"]} joined to ambrockets!",
+    })
   end
 
   def handle_message(client_socket, message)
@@ -7,5 +11,9 @@ class ChatRoomChannel < Amber::WebSockets::Channel
   end
 
   def handle_leave(client_socket)
+    ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
+      "user" => "",
+      "message" => "#{client_socket.session["user"]} left ambrockets..."
+    })
   end
 end
