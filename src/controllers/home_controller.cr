d4fdb6f9dfa1f0d9c40e4e832c51ae4a9a6108ba
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 02:03:27 -0500

    Simplify join logic

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,20 +1,15 @@
 class HomeController < ApplicationController
-  private property joined = false
-
   def index
-    if (user = session["user"]) && !joined
-      ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
-        "user" => "Server",
-        "message" => "#{user} joined to ambrockets!"
-      })
-    end
     render("index.slang")
   end
 
   def register
     if (user = params["user"]) && !user.blank?
       session["user"] = user[0..100]
-      @joined = true
+      ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
+        "user" => "Server",
+        "message" => "#{session["user"]} joined to ambrockets!"
+      })
     end
     redirect_to(HomeController, :index)
   end
@@ -25,7 +20,6 @@ class HomeController < ApplicationController
       "message" => "#{session["user"]} left ambrockets..."
     })
     session.delete("user")
-    @joined = false
     redirect_to(HomeController, :index)
   end
 end
