c9700db560fd8c8cadf9834d9a3fca305e30049c
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 00:34:36 -0500

    Add user

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,13 +1,19 @@
 class HomeController < ApplicationController
   def index
-    ChatSocket.broadcast("message", "chat_room:hello", "message_new", {"message" => "Someone is visiting ambrockets..."})
+    ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
+      "user" => "Server",
+      "message" => "Someone is visiting ambrockets..."
+    })
     render("index.slang")
   end
 
   def register
     if (user = params["user"]) && !user.blank?
       session["user"] = user[0..100]
-      ChatSocket.broadcast("message", "chat_room:hello", "message_new", {"message" => "#{session["user"]} joined to ambrockets!"})
+      ChatSocket.broadcast("message", "chat_room:hello", "message_new", {
+        "user" => "Server",
+        "message" => "#{session["user"]} joined to ambrockets!"
+      })
     end
     redirect_to(HomeController, :index)
   end
