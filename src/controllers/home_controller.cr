cdb68a6af8a5b8714fb9affaefa7b7308c5df02e
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 00:30:06 -0500

    Test broadcast method

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
+    ChatSocket.broadcast("message", "chat_room:hello", "message_new", {"message" => "Someone is visiting ambrockets..."})
     render("index.slang")
   end
 
   def register
     if (user = params["user"]) && !user.blank?
       session["user"] = user[0..100]
+      ChatSocket.broadcast("message", "chat_room:hello", "message_new", {"message" => "#{session["user"]} joined to ambrockets!"})
     end
     redirect_to(HomeController, :index)
   end
